acc30ce85e0c00c8adaae1b381d04627ae65d314
pkanchi23 <49101607+pkanchi23@users.noreply.github.com>
2024-03-08 21:15:31 -0800

    fixing feedback / the multiple SQL to NL prompt runs

--- main.py
+++ main.py
@@ -69,13 +69,70 @@ def sql_to_NL_answer(df, natural_language, pl_group_id):
     promptlayer.track.prompt(request_id=pl_id, 
         prompt_name='SQL to NL', prompt_input_variables=variables)
     
+    #group prompts
     promptlayer.track.group(
         request_id=pl_id, 
         group_id=pl_group_id
     )
+    
+    #add metadata
+    promptlayer.track.metadata(
+        request_id=pl_id,
+        metadata={
+            "User_ID":st.session_state['User_ID']
+        }
+    )
 
     NL_answer = response.choices[0].message.content
     return NL_answer, pl_id
+
+#function to handle feedback
+def handle_positive_feedback():
+    # Update session state to indicate feedback has been given
+    st.session_state['feedback_given'] = True
+    st.session_state['temp'] = True  # It seems you're using 'temp' as an additional flag; ensure its role is clear and necessary
+    
+    # Perform your feedback logic here
+    promptlayer.track.metadata(
+        request_id=st.session_state['pl_id_NL_SQL'],
+        metadata={
+            "Feedback": "Positive",
+            "User_ID":st.session_state['User_ID']
+        }
+    )
+    promptlayer.track.metadata(
+        request_id=st.session_state['pl_id_SQL_NL'],
+        metadata={
+            "Feedback": "Positive",
+            "User_ID":st.session_state['User_ID']
+        }
+    )
+    # Display success message
+    st.success("Thanks for the feedback! 👍")
+
+#function to handle feedback
+def handle_negative_feedback():
+    # Similar update for negative feedback
+    st.session_state['feedback_given'] = True
+    st.session_state['temp'] = True
+    
+    # Negative feedback logic
+    promptlayer.track.metadata(
+        request_id=st.session_state['pl_id_NL_SQL'],
+        metadata={
+            "Feedback": "Negative",
+            "User_ID":st.session_state['User_ID']
+        }
+    )
+    promptlayer.track.metadata(
+        request_id=st.session_state['pl_id_SQL_NL'],
+        metadata={
+            "Feedback": "Negative",
+            "User_ID":st.session_state['User_ID']
+        }
+    )
+    # Display error message
+    st.error("Thanks for the feedback, we're working on it!")
     
 # Connect to your SQL database
 current_dir = Path(__file__).parent
@@ -109,29 +166,6 @@ if 'display_sample' not in st.session_state or st.session_state.display_sample:
     sample_df = pd.read_sql_query(sample_query, conn)
     st.write("Original Database:", sample_df)
 
-natural_language_input = st.text_input("Enter your question:", "How many different ship names are there?")
-
-#Push feedback to CSV
-def save_feedback(natural_language_input, sql_query, feedback, ran, pl_id, user_id):
-    current_dir = Path(__file__).parent
-    feedback_data_path = str(current_dir/"feedback_data.csv")
-    df = pd.read_csv(feedback_data_path)
-    new_row = {
-            "Natural Language Question": natural_language_input, 
-            "Returned SQL": sql_query, 
-            "Feedback": feedback,
-            "Program Ran": ran
-        }
-    df = df.append(new_row, ignore_index=True)
-    df.to_csv(feedback_data_path, index=False)
-    promptlayer.track.metadata(
-        request_id=pl_id,
-        metadata={
-            "Feedback": feedback,
-            "User_ID":user_id
-        }
-    )  
-
 #add session state variables to persist in streamlit across runs 
 if 'feedback_given' not in st.session_state:
     st.session_state['feedback_given'] = False
@@ -147,6 +181,16 @@ if 'User_ID' not in st.session_state:
     st.session_state['User_ID'] = "Streamlit_"+str(random.randint(10000000, 99999999))
 if 'Group_ID' not in st.session_state:
     st.session_state['Group_ID'] = promptlayer.group.create()
+if 'Result_Generated' not in st.session_state:
+    st.session_state['Result_Generated'] = False
+if 'starter_question' not in st.session_state:
+    starter_questions = ["How many different ship names are there?", "What is the difference in the average freight cost between Lyon and Rio de Janeiro?", "What is the employee ID that handled the most orders?", "Which country received the highest number of orders?", "List all orders placed by customer LEHMS"]
+    st.session_state['starter_question'] = starter_questions[random.randint(0,5)]
+if 'temp' not in st.session_state:
+    st.session_state['temp'] = False
+
+#Choose a random starter question
+natural_language_input = st.text_input("Enter your question:", st.session_state['starter_question'])
 
 if st.button("Generate Response", key="submit"):
     st.session_state['Group_ID'] = promptlayer.group.create()
@@ -154,6 +198,7 @@ if st.button("Generate Response", key="submit"):
     st.session_state['pl_id_NL_SQL'] = pl_id_NL_SQL
     st.session_state['feedback_given'] = False
     st.session_state['program_ran'] = False
+    st.session_state['Result_Generated'] = False
     try:
         df = pd.read_sql_query(st.session_state['SQL_query'], conn)
         st.session_state['program_ran'] = True
@@ -162,6 +207,13 @@ if st.button("Generate Response", key="submit"):
             request_id=pl_id_NL_SQL,
             score=100
         )
+        #add metadata
+        promptlayer.track.metadata(
+            request_id=st.session_state['pl_id_NL_SQL'],
+            metadata={
+                "User_ID":st.session_state['User_ID']
+            }
+        )  
     except Exception as e:
         st.session_state['program_ran'] = False
         #score 0 if the SQL did not run
@@ -169,39 +221,41 @@ if st.button("Generate Response", key="submit"):
             request_id=pl_id_NL_SQL,
             score=0
         )
-        save_feedback(natural_language_input, st.session_state['SQL_query'], "Negative", st.session_state['program_ran'], st.session_state['pl_id_NL_SQL'], st.session_state['User_ID'])
+        #add the metadata
+        promptlayer.track.metadata(
+            request_id=st.session_state['pl_id_NL_SQL'],
+            metadata={
+                "User_ID":st.session_state['User_ID']
+            }
+        )  
         st.error(f"Error executing query: {e}")
         
-    
 # Ensure the SQL result remains displayed after feedback
-if st.session_state['program_ran']:
-    # st.write("SQL Query: %s" % (st.session_state['SQL_query']))
+if st.session_state['program_ran'] and not st.session_state['Result_Generated']:
+    # st.write("SQL Query: %s" % (st.session_state['SQL_query'])) #uncomment this to write out the SQL Query
     try:
         df = pd.read_sql_query(st.session_state['SQL_query'], conn)
-        NL_answer, _ = sql_to_NL_answer(df, natural_language_input, st.session_state['Group_ID'])
-        st.write(df)
-        st.write(NL_answer) #NL Answer, could also write out df, to show the selected df
+        NL_answer, st.session_state['pl_id_SQL_NL']  = sql_to_NL_answer(df, natural_language_input, st.session_state['Group_ID'])
+        st.session_state['Result_Generated'] = True #issued
+        #add the metadata
+        promptlayer.track.metadata(
+            request_id=st.session_state['pl_id_NL_SQL'],
+            metadata={
+                "User_ID":st.session_state['User_ID']
+            }
+        )  
+        st.write(df) #write section of database used to answer question
+        st.write(NL_answer) #write the question in natural language
     except Exception as e:
-        st.error(f"Error re-displaying query result: {e}")
+        st.error(f"Error displaying query result: {e}")
         st.session_state['program_ran'] = False
-        save_feedback(natural_language_input, st.session_state['SQL_query'], "Negative", st.session_state['program_ran'], st.session_state['pl_id_NL_SQL'], st.session_state['User_ID'])
-    
-feedback_placeholder = st.empty()
-
-#Feedback buttons
-if st.session_state['program_ran']:
+   
+# Conditional rendering of feedback buttons
+if st.session_state['program_ran'] and not st.session_state['feedback_given']:
     st.write("Rate your result:")
     thumbs_up, thumbs_down = st.columns(2)
-    if thumbs_up.button("👍", key="thumbs_up") and not st.session_state['feedback_given']:
-        save_feedback(natural_language_input, st.session_state['SQL_query'], "Positive", st.session_state['program_ran'], st.session_state['pl_id_NL_SQL'], st.session_state['User_ID'])
-        st.success("Thanks for the feedback! 👍")
-        st.session_state['feedback_given'] = True
-        
-    if thumbs_down.button("👎", key="thumbs_down") and not st.session_state['feedback_given']:
-        save_feedback(natural_language_input, st.session_state['SQL_query'], "Negative", st.session_state['program_ran'], st.session_state['pl_id_NL_SQL'], st.session_state['User_ID'])
-        st.error("Thanks for the feedback, we're working on it!")
-        st.session_state['feedback_given'] = True
-        
+    thumbs_up.button("👍", key="thumbs_up", on_click=handle_positive_feedback)
+    thumbs_down.button("👎", key="thumbs_down", on_click=handle_negative_feedback)
 
 # Close the connection to the database
 conn.close()
